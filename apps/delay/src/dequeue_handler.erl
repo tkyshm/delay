74cc20155768e3ae025819ad4d76f1b0bf7fd2cd
tkyshm <tkyshm@gmail.com>
2017-03-06 03:40:33 +0900

    long polling

--- apps/delay/src/dequeue_handler.erl
+++ apps/delay/src/dequeue_handler.erl
@@ -1,20 +1,26 @@
 -module(dequeue_handler).
 
 -export([init/2]).
--export([content_types_provided/2]).
+-export([info/3]).
 -export([dequeue/2]).
 
 -include("schema.hrl").
 
-init(Req, Opts) ->
-    {cowboy_rest, Req, Opts}.
+-define(DEFAULT_COUNT, 50).
+-define(SERVER_INTERVAL, 200).
 
-content_types_provided(Req, State) ->
-    {[
-      {<<"application/json">>, dequeue}
-     ], Req, State}.
+init(Req0, _) ->
+    Req1 = cowboy_req:stream_reply(200, #{<<"content-type">> => <<"application/json">>}, Req0),
+    erlang:send_after(200, self(), timeout),
+    {cowboy_loop, Req1, ?DEFAULT_COUNT}. %% TODO: enable to set count
 
-dequeue(Req, State) ->
+info(timeout, Req, 0) ->
+    {stop, Req, 0};
+info(timeout, Req, Count) ->
+    erlang:send_after(?SERVER_INTERVAL, self(), timeout),
+    dequeue(Req, Count).
+
+dequeue(Req, Count) ->
     case m:find_all_ready_jobs() of
         {ok, Jobs} ->
             lists:foreach(fun(J) ->
@@ -24,8 +30,10 @@ dequeue(Req, State) ->
                 #{<<"data">> => J#job.data, <<"uid">> => J#job.uid}
             end, Jobs),
             Resp = jiffy:encode(JobJson),
-            {stop, cowboy_req:reply(200, #{}, Resp, Req), State};
+            cowboy_req:stream_body(Resp, nofin, Req),
+            {ok, Req, Count - 1};
         _ ->
             Resp = <<"{\"error\":\"Internal Server Error\"}">>,
-            {stop, cowboy_req:reply(500, #{}, Resp, Req), State}
+            cowboy_req:stream_body(Resp, nofin, Req),
+            {ok, cowboy_req:reply(500, #{}, Resp, Req), Count}
     end.
